69eaf936dc7900c3a5817c0b1d457c356cd03f02
Jacob Roberts <j2late@gmail.com>
2024-10-30 13:20:33 -0500

    tested features and fixed home page

--- project/store/urls.py
+++ project/store/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 
 from . import views
 
-app_name = "accounts"
+app_name = "store"
+
 
 urlpatterns = [
-    path("products", views.search_product,name="product_search")
+    
+    path("products/", views.search_product,name="product_search")
 ]
 
